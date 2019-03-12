388c82fbca251be196ac0888052ffb8909d37c81
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2019-03-12 12:45:23 -0300

    Add files via upload

---
+++ Exeercicios_Praticos/algorithms.py
@@ -0,0 +1,40 @@
+#coding: utf-8
+
+import sys
+
+# Esse metodo recebe uma lista com as matriculas dos alunos
+# e retorna essa lista em ordem crescente de matriculas
+def retorna_matriculas_decrescente(alist):
+	for j in xrange(len(alist)):
+		for i in xrange(len(alist)-1):
+			if alist[i] > alist[i+1]:
+				alist[i], alist[i+1] = alist[i+1], alist[i]
+	return alist
+
+# Esse metodo recebe e valor para ser dado o troco e uma lista com os tipos de moedas possiveis
+# e retorna o numero minimo de moedas possiveis em que o troco pode ser dado
+
+# Caso o valor não possa ser alcançado pela combinação de moedas o valor -1 é retornado Ex: valor = 11  moedas = {5, 10, 25}
+# Assuma que existe uma quantidade infinita de cada tipo de moeda
+def retorna_minimo_moedas(valor, tipos_moedas):
+	print valor, tipos_moedas
+	resultado = retorna_minimo_moedas_FB(tipos_moedas, valor)
+	if resultado == sys.maxint:
+		return -1
+	else:
+		return resultado
+
+def retorna_minimo_moedas_FB(tipos_moedas, valor):
+	if valor == 0:
+		return 0
+	
+	resultado = sys.maxint
+	
+	for moeda in tipos_moedas:
+		
+		if (moeda <= valor):
+			resultado = min(resultado, retorna_minimo_moedas_FB(tipos_moedas, valor - moeda) + 1) 
+		
+	return resultado	 
+
+
